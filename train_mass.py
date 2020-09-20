1d5b1b519178ae091d3b5f4b5c491ac2c3e775a7
rtpsec <56045567+rtpsec@users.noreply.github.com>
2020-09-20 10:21:01 -0400

    Update train_mass.py

--- train_mass.py
+++ train_mass.py
@@ -22,4 +22,18 @@ def get_force(mass, acceleration):
     return mass_acceleration
 train_force = get_force(train_mass,train_acceleration)
 
-print("The GE train supplies " + str(train_force) + " Newtons of force.")
+
+def get_energy(mass, c=3*10**8):
+    return mass * c **2
+  
+bomb_energy = get_energy(bomb_mass,)
+print(" A 1kg bomb supplies " + str(bomb_energy) + " Joules")
+
+
+def get_work(mass,acceleration,distance):
+  force=get_force(mass,acceleration)
+  return force * distance
+
+train_work = get_work(train_mass,train_acceleration, train_distance)
+
+print("The GE train does " + str(train_work) + " Joules of work over " + str(train_distance) + "meters")
