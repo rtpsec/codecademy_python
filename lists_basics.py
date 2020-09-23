3eac1163d0c7b489d2ccb2222e23b7c240ad6325
rtpsec <56045567+rtpsec@users.noreply.github.com>
2020-09-23 08:29:53 -0400

    Create lists_basics.py

---
+++ lists_basics.py
@@ -0,0 +1,10 @@
+ints_and_strings = [1, 2, 3, 'four', 'five', 'any']
+sam_height=['Sam', 67]
+
+# Add to an empty list
+langs.append("Python")
+langs.append("Perl")
+
+# Add to the end of the list
+langs.insert(0, "PHP")
+langs.insert(2, "Lua")
