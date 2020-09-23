4ae6306acf79cb6a18288ae750c4ed0d94271e3c
rtpsec <56045567+rtpsec@users.noreply.github.com>
2020-09-23 08:34:55 -0400

    Update lists_basics.py

--- lists_basics.py
+++ lists_basics.py
@@ -8,3 +8,8 @@ langs.append("Perl")
 # Add to the end of the list
 langs.insert(0, "PHP")
 langs.insert(2, "Lua")
+
+# Multiple Lists in one list
+heights = [['Jenny', 61], ['Alexus', 70], ['Sam', 67], ['Grace', 64], ['Vik', 68]]
+ages = [['Aaron', 15], ['Dhruti', 16]]
+
