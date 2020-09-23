8aedce16689bd299d8351460182df27f2b1277c3
rtpsec <56045567+rtpsec@users.noreply.github.com>
2020-09-23 08:38:44 -0400

    Update lists_basics.py

--- lists_basics.py
+++ lists_basics.py
@@ -13,3 +13,12 @@ langs.insert(2, "Lua")
 heights = [['Jenny', 61], ['Alexus', 70], ['Sam', 67], ['Grace', 64], ['Vik', 68]]
 ages = [['Aaron', 15], ['Dhruti', 16]]
 
+#zip takes two (or more) lists as inputs and returns an object that contains a list of pairs. Each pair contains one element from each of the inputs.
+names = ['Jenny', 'Alexus', 'Sam', 'Grace']
+dogs_names = ['Elphonse', 'Dr. Doggy DDS', 'Carter', 'Ralph']
+
+names_and_dogs_names = zip(names, dogs_names)
+
+print(names_and_dogs_names)
+# output example
+# <zip object at 0x7fe996ee7b08>
