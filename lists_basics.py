b1702f70c92b858e87abac7f07f1c6c5ddba452b
rtpsec <56045567+rtpsec@users.noreply.github.com>
2020-09-23 08:45:12 -0400

    Update lists_basics.py

--- lists_basics.py
+++ lists_basics.py
@@ -2,8 +2,12 @@ ints_and_strings = [1, 2, 3, 'four', 'five', 'any']
 sam_height=['Sam', 67]
 
 # Add to an empty list
-langs.append("Python")
-langs.append("Perl")
+
+orders = ['daisies', 'periwinkle']
+orders.append('tulips')
+orders.append('roses')
+print(orders)
+
 
 # Add to the end of the list
 langs.insert(0, "PHP")
